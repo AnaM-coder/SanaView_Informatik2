5e921ca9e8d3e067ac54489200fd0b5d6a640377
AnaM-coder <andraana@students.zhaw.ch>
2025-04-23 13:52:04 +0200

    HB

--- pages/Laborwerte.py
+++ pages/Laborwerte.py
@@ -33,8 +33,6 @@ laboroptionen = {
     "CRP": {"einheit": "mg/L", "ref_min": 0, "ref_max": 10},
     "TSH": {"einheit": "mIU/L", "ref_min": 0.4, "ref_max": 4.0},
     "Glucose": {"einheit": "mg/dL", "ref_min": 70, "ref_max": 99}
-    "HBA1c (Nichtdiabetiker)": {"einheit": "%", "ref_min": 4.0, "ref_max": 6.0},
-}
 
 ausgewählt = st.selectbox("Laborwert", list(laboroptionen.keys()))
 einheit = laboroptionen[ausgewählt]["einheit"]
