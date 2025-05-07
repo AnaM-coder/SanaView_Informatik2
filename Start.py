a31c231d0f63ce96bf6402b854801641444ba69b
CristianaPBastos <cristianabastos.98@gmail.com>
2025-05-08 00:07:24 +0200

    ..

--- Start.py
+++ Start.py
@@ -11,15 +11,14 @@ data_manager = DataManager(fs_protocol='webdav', fs_root_folder="SanaView2")
 login_manager = LoginManager(data_manager)
 
 # === Login anzeigen (im Hauptbereich) ===
-login_manager.authenticator.login("Login", location="main")
+login_manager.authenticator.login("main")  # FIXED!
 
 # === Wenn nicht eingeloggt → abbrechen ===
 if not st.session_state.get("authentication_status", False):
     st.stop()
 
 # === Logout nur in der Sidebar anzeigen ===
-login_manager.authenticator.logout("Logout", location="sidebar")
-
+login_manager.authenticator.logout("Logout", "sidebar")
 # === Logo links oben anzeigen ===
 if os.path.exists("img/sanaview_logo.png"):
     st.image("img/sanaview_logo.png", width=250)
