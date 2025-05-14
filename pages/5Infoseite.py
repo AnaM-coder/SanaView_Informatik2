b73ab063f10f072fd4cc12c7b4baf42b565528a1
CristianaPBastos <cristianabastos.98@gmail.com>
2025-05-14 13:17:52 +0200

    korrektur

--- pages/5Infoseite.py
+++ pages/5Infoseite.py
@@ -6,11 +6,10 @@ from utils.login_manager import LoginManager
 # === Seitenlayout ===
 st.set_page_config(page_title="Info-Seite", layout="wide")
 
-# === Hintergrundfarbe setzen ===
 st.markdown("""
     <style>
         html, body, [data-testid="stAppViewContainer"], [data-testid="stAppViewContainer"] > .main {
-            background-color: #f0f8ff !important;
+            background-color: #d9ecf2 !important;
         }
     </style>
 """, unsafe_allow_html=True)
