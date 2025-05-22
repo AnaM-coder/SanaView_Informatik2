c52e3b1159024c781080fcef87f4955a03f0cb2b
CristianaPBastos <cristianabastos.98@gmail.com>
2025-05-22 23:00:20 +0200

    ..

--- pages/1Hauptmenü.py
+++ pages/1Hauptmenü.py
@@ -52,19 +52,19 @@ st.markdown("""
 # === Abschluss ===
 st.markdown("<p style='margin-top: 25px; font-size:18px;'><strong>Behalten Sie Ihre Gesundheit im Blick – einfach, sicher und übersichtlich ✨.</strong></p>", unsafe_allow_html=True)
 
-cols = st.columns(7)
-with cols[1]:
+col1, col2, col3, col4, col5 = st.columns(5)
+with col1:
     if st.button("Profil verwalten"):
         st.switch_page("pages/2Profilverwaltung.py")
-with cols[2]:
+with col2:
     if st.button("Laborwerte – Eingabe"):
         st.switch_page("pages/3Laborwerte.py")
-with cols[3]:
+with col3:
     if st.button("Verlauf anzeigen"):
         st.switch_page("pages/4Verlauf.py")
-with cols[4]:
+with col4:
     if st.button("Infoseite"):
         st.switch_page("pages/5Infoseite.py")
-with cols[5]:
+with col5:
     if st.button("Start"):
         st.switch_page("Start.py")
