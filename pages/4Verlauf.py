e5fc1c3257500874e665328bfa0e5d08d7c49bff
CristianaPBastos <cristianabastos.98@gmail.com>
2025-05-22 19:32:43 +0200

    .

--- pages/4Verlauf.py
+++ pages/4Verlauf.py
@@ -13,7 +13,7 @@ def get_base64_of_bin_file(bin_file):
         data = f.read()
     return base64.b64encode(data).decode()
 
-img_path = "img/Verlauf_bg.png"
+img_path = "img/labor.png"
 img_base64 = get_base64_of_bin_file(img_path)
 
 st.markdown(
