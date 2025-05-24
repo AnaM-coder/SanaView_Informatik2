088fa5d50fc050f7b16e715a8a424a5a44efd119
CristianaPBastos <cristianabastos.98@gmail.com>
2025-05-25 01:50:40 +0200

    --

--- pages/3Laborwerte.py
+++ pages/3Laborwerte.py
@@ -310,13 +310,13 @@ if len(df) > 0:
                 df = df[~maske].reset_index(drop=True)
                 st.session_state[session_key] = df
                 data_manager.save_data(session_state_key=session_key)
-                st.toast("Eintrag erfolgreich gelöscht.")
+                st.success("Eintrag erfolgreich gelöscht.")
                 st.session_state["delete_confirm"] = False
                 st.session_state["delete_result"] = "success"
                 st.rerun()
         with col3:
             if st.button("❌ Nein", key="delete_no"):
-                st.toast("Löschvorgang abgebrochen.")
+                st.info("Löschvorgang abgebrochen.")
                 st.session_state["delete_confirm"] = False
                 st.session_state["delete_result"] = "cancel"
                 st.rerun()
